6c9ba766f21935c5117f047dcb327d63b8e96e83
qasimky <qasimkycs@gmail.com>
2023-12-07 08:54:39 -0800

    adding user table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_13_185845) do
+ActiveRecord::Schema.define(version: 2023_12_07_164250) do
 
   create_table "articals", force: :cascade do |t|
     t.string "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2023_11_13_185845) do
     t.string "name"
     t.text "description"
     t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
