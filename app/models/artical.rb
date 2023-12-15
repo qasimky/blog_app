fceafe181741f216e44e760a289587a5506d54b8
qasimky <qasimkycs@gmail.com>
2023-12-15 08:42:24 -0800

    5th

--- app/models/artical.rb
+++ app/models/artical.rb
@@ -1,4 +1,5 @@
 class Artical < ApplicationRecord
+    belong_to :user
     validates :title, presence: true, length: {minimum: 5, maximum:20}
     validates :description, presence: true, length: {minimum: 15, maximum:200}
 end
